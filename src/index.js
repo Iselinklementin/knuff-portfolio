1b7c703a8f3263932139803ed4e07a6bae3ae039
Iselinklementin <69785627+Iselinklementin@users.noreply.github.com>
2022-01-25 14:18:03 +0100

    fixed scroll

--- src/index.js
+++ src/index.js
@@ -5,16 +5,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Portfolio from "./components/routes/portfolio";
 import Websites from "./components/routes/websites";
 import About from "./components/routes/about";
+import ScrollToTop from "./components/constant/ScrollToTop";
 
 const rootElement = document.getElementById("root");
 render(
   <BrowserRouter>
-    <Routes>
-      <Route exact path="/" element={<App />} />
-      <Route exact path="portfolio" element={<Portfolio />} />
-      <Route exact path="websites" element={<Websites />} />
-      <Route exact path="about" element={<About />} />
-    </Routes>
+    <ScrollToTop>
+      <Routes>
+        <Route exact path="/" element={<App />} />
+        <Route exact path="portfolio" element={<Portfolio />} />
+        <Route exact path="websites" element={<Websites />} />
+        <Route exact path="about" element={<About />} />
+      </Routes>
+    </ScrollToTop>
   </BrowserRouter>,
   rootElement
 );
