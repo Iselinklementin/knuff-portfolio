06b18b1e8f7d9c188d2029ad298617e550e0df57
Iselinklementin <69785627+Iselinklementin@users.noreply.github.com>
2022-01-25 11:55:02 +0100

    added intro design

--- src/components/routes/portfolio.jsx
+++ src/components/routes/portfolio.jsx
@@ -9,29 +9,38 @@ export default function Portfolio() {
       <div className="wrapper design">
         <Nav />
         <Container>
-          <h2>Graphic design</h2>
+          <h3 className="mobile graphic">Graphic design</h3>
           <h1>Portfolio</h1>
+          <h3 className="tablet graphic">Graphic design</h3>
           <div className="top-icon"></div>
-        </Container>
 
-        <img src={Mugs} alt="" className="image" />
+          <div className="image-container">
+            <div className="clouds"></div>
+            <img src={Mugs} alt="" className="image" />
+            <div className="background-tablet"></div>
+          </div>
+        </Container>
 
         <div className="background-container">
           <div className="background"></div>
         </div>
-        <div className="head-line"></div>
-        <p className="text">It’s not about decorating, but communicating.</p>
-        <div className="text-container">
-          <p>
-            Most of my time I have been creating and working closely with typography, layout, and all of the details in
-            between.
-          </p>
 
-          <p>
-            I bring experience, creativity and understanding to every project I work on, and I’ve been fortunate to work
-            with several companies to develop my skill set. I have chosen a few of my projects here, take a look around.
-          </p>
-        </div>
+        <Container>
+          <div className="head-line"></div>
+          <p className="text">It’s not about decorating, but communicating.</p>
+          <div className="text-container">
+            <p>
+              Most of my time I have been creating and working closely with typography, layout, and
+              all of the details in between.
+            </p>
+
+            <p>
+              I bring experience, creativity and understanding to every project I work on, and I’ve
+              been fortunate to work with several companies to develop my skill set. I have chosen a
+              few of my projects here, take a look around.
+            </p>
+          </div>
+        </Container>
       </div>
       <Footer />
     </>
--- src/components/routes/websites.jsx
+++ src/components/routes/websites.jsx
@@ -9,27 +9,38 @@ export default function Websites() {
       <div className="wrapper websites">
         <Nav />
         <Container>
-          <h2>Frontend development</h2>
+          <h3 className="mobile frontend">Frontend development</h3>
           <h1>Website</h1>
+          <h3 className="tablet frontend">Frontend development</h3>
           <div className="top-icon"></div>
+
+          <div className="image-container">
+            <img src={iPhone} alt="" className="image" />
+            <div className="image-icons"></div>
+          </div>
         </Container>
-        <img src={iPhone} alt="" className="image" />
 
         <div className="background-container">
           <div className="background"></div>
         </div>
-        <div className="head-line"></div>
-        <p className="text">The space where art and science collide.</p>
 
-        <div className="text-container">
-          <p>I want to bring both the technical and visual aspects of digital products to life.</p>
+        <Container>
+          <div className="head-line"></div>
+          <p className="text">The space where art and science collide.</p>
 
-          <p>
-            I am a fast learner and have a strong desire to use my new skills in frontend web development. UX, beautiful
-            pixels and writing clean accessible code matters to me. I hope that my projects here shows at least a small
-            part of my enthusiasm for the craft. Have a look at my school projects.
-          </p>
-        </div>
+          <div className="text-container">
+            <p>
+              I want to bring both the technical and visual aspects of digital products to life.
+            </p>
+
+            <p>
+              I am a fast learner and have a strong desire to use my new skills in frontend web
+              development. UX, beautiful pixels and writing clean accessible code matters to me. I
+              hope that my projects here shows at least a small part of my enthusiasm for the craft.
+              Have a look at my school projects.
+            </p>
+          </div>
+        </Container>
       </div>
 
       <Footer />
