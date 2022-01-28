0bc4b670f3214671e748b9cbfa6a041643e488b0
Iselinklementin <69785627+Iselinklementin@users.noreply.github.com>
2022-01-28 14:40:05 +0100

    image home

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./sass/style.scss";
 import Footer from "./components/layout/Footer";
 import Nav from "./components/layout/Nav";
 import Bird from "./images/home/bird.jpg";
+import Manet from "./images/home/manet.jpg";
 import Me from "./images/about-me.jpg";
 // import React from "react";
 import List from "./components/ui/List";
@@ -12,12 +13,18 @@ import Loader from "./components/ui/Loader";
 import React, { useState, useEffect } from "react";
 
 export default function App() {
-  const designList = ["Adobe InDesign", "Adobe Illustrator", "Adobe Photoshop", "Adobe XD", "Procreate"];
+  const designList = [
+    "Adobe InDesign",
+    "Adobe Illustrator",
+    "Adobe Photoshop",
+    "Adobe XD",
+    "Procreate",
+  ];
   const developerList = ["Sass", "React", "Bootstrap", "WordPress API", "Strapi API"];
 
   const [isLoading, setLoading] = useState(true);
   function fakeRequest() {
-    return new Promise((resolve) => setTimeout(() => resolve(), 1700));
+    return new Promise(resolve => setTimeout(() => resolve(), 1700));
   }
 
   useEffect(() => {
@@ -52,7 +59,10 @@ export default function App() {
           <section className="intro-section">
             <h3>Nice to meet you</h3>
             <div className="code-icon"></div>
-            <p>I love clean code, creating strategies, problem solving and design that makes people act.</p>
+            <p>
+              I love clean code, creating strategies, problem solving and design that makes people
+              act.
+            </p>
             <p>I’m at my happiest when I’m creating, learning & exploring.</p>
           </section>
 
@@ -86,9 +96,9 @@ export default function App() {
               Designer<span>.</span>
             </h3>
             <p>
-              I’m a graphic designer and I’ve worked in all areas of branding and design for print. I have designed
-              small business cards, large bus wrappings - and everything between. Take a look at my portfolio to see
-              some of my work.
+              I’m a graphic designer and I’ve worked in all areas of branding and design for print.
+              I have designed small business cards, large bus wrappings - and everything between.
+              Take a look at my portfolio to see some of my work.
             </p>
             <div className="keywords-desktop">
               <p>Colours</p>
@@ -111,9 +121,9 @@ export default function App() {
 
           <div className="description description-tablet">
             <p>
-              I’m a graphic designer and I’ve worked in all areas of branding and design for print. I have designed
-              small business cards, large bus wrappings - and everything between. Take a look at my portfolio to see
-              some of my work.
+              I’m a graphic designer and I’ve worked in all areas of branding and design for print.
+              I have designed small business cards, large bus wrappings - and everything between.
+              Take a look at my portfolio to see some of my work.
             </p>
             <div className="keywords-desktop">
               <p>Colours</p>
@@ -133,7 +143,7 @@ export default function App() {
 
         <section className="developer card">
           <div className="card-top">
-            <img src={Bird} className="card-img" />
+            <img src={Manet} className="card-img" />
             <div className="card-divider"></div>
             <h3 className="card-developer-heading">Developer</h3>
 
@@ -155,8 +165,9 @@ export default function App() {
               Developer<span>.</span>
             </h3>
             <p>
-              Through my studies at Noroff I have gotten exposure to a wide range of web development skills. I’d love to
-              work with forward-thinking people to design and build interactive, accessible websites and products.
+              Through my studies at Noroff I have gotten exposure to a wide range of web development
+              skills. I’d love to work with forward-thinking people to design and build interactive,
+              accessible websites and products.
             </p>
             <div className="keywords-desktop">
               <p>Meaningful</p>
@@ -180,8 +191,9 @@ export default function App() {
 
           <div className="description description-tablet">
             <p>
-              Through my studies at Noroff I have gotten exposure to a wide range of web development skills. I’d love to
-              work with forward-thinking people to design and build interactive, accessible websites and products.
+              Through my studies at Noroff I have gotten exposure to a wide range of web development
+              skills. I’d love to work with forward-thinking people to design and build interactive,
+              accessible websites and products.
             </p>
             <div className="keywords-desktop">
               <p>Meaningful</p>
@@ -210,15 +222,17 @@ export default function App() {
 
           <div className="about-text">
             <h2>Hello.</h2>
-            <p className="about-intro">I’m a designer, frontend developer, gamer and puzzle enthusiast.</p>
+            <p className="about-intro">
+              I’m a designer, frontend developer, gamer and puzzle enthusiast.
+            </p>
             <p>
-              I’m based in Sandefjord and have two kids. After 8 years working as a graphic designer it was time to
-              explore and evolve, so I went back to school to learn how to code!
+              I’m based in Sandefjord and have two kids. After 8 years working as a graphic designer
+              it was time to explore and evolve, so I went back to school to learn how to code!
             </p>
 
             <p>
-              I believe my experience working as a designer is a great strength in development. Both with UX and
-              creating UI - but also when writing short, meaningful code.
+              I believe my experience working as a designer is a great strength in development. Both
+              with UX and creating UI - but also when writing short, meaningful code.
             </p>
 
             <Link to="/about" className="about-btn mobile-btn">
